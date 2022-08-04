1861533a338cfd4a7ddcdc2394488aaeb594ce4e
unknown <eske17.ea@gmail.com>
2022-08-04 11:11:13 +0300

    removed is_admin

--- core/models.py
+++ core/models.py
@@ -67,7 +67,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=128, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(
